3f208e3139d85a0c3cdb38d567802f1fa8cde491
eran <eranka12@gmail.com>
2025-02-25 15:57:59 +0200

    Created UI module , with BaseScreen strings and colors.

--- composeApp/src/commonMain/kotlin/org/example/hit/heal/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/hit/heal/App.kt
@@ -1,9 +1,8 @@
 package org.example.hit.heal
 
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import org.example.hit.heal.core.presentation.SampleScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import org.example.hit.heal.presentaion.SampleScreen
 
 @Composable
 @Preview
--- composeApp/src/commonMain/kotlin/org/example/hit/heal/presentaion/Color.kt
+++
@@ -1,7 +0,0 @@
-package org.example.hit.heal.presentaion
-
-import androidx.compose.ui.graphics.Color
-
-val primaryColor = Color(0xFF6FCF97)
-val backgroundColor = Color(0xFFDFF5E9)
-
