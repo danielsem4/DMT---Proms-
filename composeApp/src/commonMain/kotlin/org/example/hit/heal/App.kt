faf963597bb9098f20b2ca1c9a68e191677e1e93
osherk2001 <osherk1975@gmail.com>
2025-05-28 00:58:07 +0300

    audio button

--- composeApp/src/commonMain/kotlin/org/example/hit/heal/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/hit/heal/App.kt
@@ -3,6 +3,8 @@ package org.example.hit.heal
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
+import org.example.hit.heal.oriantation.feature.presentation.DrawScreen
+import org.example.hit.heal.oriantation.feature.presentation.FeedbackScreen
 import org.example.hit.heal.oriantation.feature.presentation.ShapesDragScreen
 
 
@@ -10,7 +12,7 @@ import org.example.hit.heal.oriantation.feature.presentation.ShapesDragScreen
 fun App(context: Any? = null) {
 //    NavigationGraph()
     MaterialTheme {
-       Navigator(ShapesDragScreen())
+       Navigator(FeedbackScreen())
 
 
     }
---
+++ oriantation/src/desktopMain/kotlin/org/example/hit/heal/oriantation/feature/presentation/components/AudioPlayer.desktop.kt
@@ -0,0 +1,6 @@
+package org.example.hit.heal.hitber.presentation.understanding.components
+
+actual class AudioPlayer actual constructor() {
+    actual fun play(url: String, onCompletion: () -> Unit) {
+    }
+}
