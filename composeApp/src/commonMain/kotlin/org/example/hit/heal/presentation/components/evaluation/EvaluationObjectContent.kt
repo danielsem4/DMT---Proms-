392e8b4d23ff5278d797d4b7973622ea49e5c612
Daniel sem <klutz@Daniels-MacBook-Pro.local>
2025-08-14 17:46:20 +0300

    changed the select question ui

--- composeApp/src/commonMain/kotlin/org/example/hit/heal/presentation/components/evaluation/EvaluationObjectContent.kt
+++ composeApp/src/commonMain/kotlin/org/example/hit/heal/presentation/components/evaluation/EvaluationObjectContent.kt
@@ -1,13 +1,10 @@
 package org.example.hit.heal.presentation.components.evaluation
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,17 +13,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import core.data.model.evaluation.EvaluationAnswer
 import core.data.model.evaluation.EvaluationObject
 import core.data.model.evaluation.EvaluationValue
 import core.data.model.evaluation.toRawString
-import dmt_proms.composeapp.generated.resources.Res
-import dmt_proms.composeapp.generated.resources.slider_value_prefix
-import org.example.hit.heal.core.presentation.FontSize.EXTRA_MEDIUM
 import org.example.hit.heal.core.presentation.FontSize.EXTRA_MEDIUM_LARGE
 import org.example.hit.heal.core.presentation.Sizes.paddingSm
 import org.example.hit.heal.core.presentation.components.CustomMultilineTextField
@@ -40,8 +32,10 @@ import org.example.hit.heal.core.presentation.components.RoundedFilledSlider
 import org.example.hit.heal.core.presentation.components.SelectionMode
 import org.example.hit.heal.core.presentation.components.dialogs.MultiSelectPillDialog
 import org.example.hit.heal.core.presentation.formatLabel
-import org.example.hit.heal.core.presentation.primaryColor
-import org.jetbrains.compose.resources.stringResource
+
+/**
+ *
+ */
 
 @Composable
 fun EvaluationObjectContent(
